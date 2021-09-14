d6ac58f682fcbc9deebd1727eb9f4cc419375036
Maksym <maksym@curatesolutions.com>
2021-09-14 01:19:59 -0500

    Uses mpsc to send data between threads

--- src/stats.rs
+++ src/stats.rs
@@ -1,9 +1,29 @@
-pub fn stats(is_silent: bool, num_read: usize, total_bytes: &mut usize, is_last: bool) {
-    *total_bytes += num_read;
-    if !is_silent {
-        eprint!("\r{}", total_bytes);
-        if is_last {
-            eprintln!("\r{}", total_bytes);
+use std::io::Result;
+use std::sync::mpsc::{Receiver, Sender};
+
+pub fn stats_loop(
+    is_silent: bool,
+    stats_rx: Receiver<Vec<u8>>,
+    write_tx: Sender<Vec<u8>>,
+) -> Result<()> {
+    let mut total_bytes = 0;
+    loop {
+        let buffer = stats_rx.recv().unwrap();
+        let num_bytes = buffer.len();
+        total_bytes += num_bytes;
+        if !is_silent {
+            eprint!("\r{}", total_bytes);
+        }
+        if write_tx.send(buffer).is_err() {
+            break;
+        }
+        if num_bytes == 0 {
+            break;
         }
     }
+
+    if !is_silent {
+        eprintln!("\r{}", total_bytes);
+    }
+    Ok(())
 }
